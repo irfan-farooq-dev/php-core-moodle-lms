893ac30b12ce9e02fc7404bdec68319fc3849bee
Irfan Farooq <irfanf33@gmail.com>
2026-02-26 16:45:31 +0500

    Moodle project files

---
+++ server/moodledata/localcache/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+// ********** This file is generated DO NOT EDIT **********
+$CFG->siteidentifier = 'PyQfMuYCNkpcAVGFEdESPwEENEqntcoBlocalhost';
+$CFG->bootstraphash = 'e76e020bfe5ea09e37d0fa77d7b74480';
+// Only if the file is not stale and has not been defined.
+if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
+    define('SYSCONTEXTID', 1);
+}
